a56234338ec359bff9a5be99ff4510358fd7fdd6
RagipatiRafi <146105040+RagipatiRafi@users.noreply.github.com>
2023-09-28 18:06:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+let x = "Rafi";
+var y = "Ragipati";
+console.log(x);           
+console.log(x + ":" +y);
+document.write(typeof x+"<br>");
+document.write(typeof y+"<br>");
+
+let a = "RAfi";
+var b = 9642920152;
+const c = 407.35;
+
+console.log(a);
+console.log(b);
+console.log(c)
+console.log(a +","+b +',' +c);
+
+document.write(typeof a+"<br>");
+document.write(typeof b+"<br>");
+document.write(typeof c+"<br>");
+
+
+
+var e = 25;
+let f = true;
+const g = 45.5
+let h ;
+var  i= ['apple','Banana','Orange','Rafi','Ragipati'];
+var j = undefined;
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+
+document.write(typeof e+"<br>");
+document.write(typeof f+"<br>");
+document.write(typeof g+"<br>");
+document.write(typeof h+"<br>");
+document.write(typeof i+"<br>");
+document.write(typeof j+"<br>");
+
+
+var myArray=['Mango','Apple','Orange','Banana','Grape'];
+console.log(myArray[0,3]);
+document.write(typeof myArray+"<br>");
+
+
+const isNameRafi=true;
+console.log(isNameRafi);
+document.write(typeof isNameRafi);
+
+
+
+
+
+
